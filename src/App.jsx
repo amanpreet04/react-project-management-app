940f3153341104ebd13b322445745ef397da7df7
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-07-15 22:27:45 +0530

    Added changes to handle starte and forward props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,64 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import SideBar from "./components/SideBar";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((previousState) => {
+      return {
+        ...previousState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleCancelAddProject() {
+    setProjectsState((previousState) => {
+      return {
+        ...previousState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  function handleAddProject(project) {
+    const newProject = {
+      ...project,
+      id: Math.random(),
+    };
+
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = (
+      <NewProject toAdd={handleAddProject} toCancel={handleCancelAddProject} />
+    );
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SideBar />
-      <NewProject />
+      <SideBar
+        onStartAddProject={handleStartAddProject}
+        projectsState={projectsState}
+      />
+      {content}
     </main>
   );
 }
